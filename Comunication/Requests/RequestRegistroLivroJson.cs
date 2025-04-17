85c83ca6d273df9c71080c5f3902c054d265f980
cantesani <cantesani@hotmail.com>
2025-04-17 00:08:42 -0300

    alteracao

--- Comunication/Requests/RequestRegistroLivroJson.cs
+++ Comunication/Requests/RequestRegistroLivroJson.cs
@@ -2,6 +2,7 @@
 {
     public class RequestRegistroLivroJson
     {
+
         public string Titulo { get; set; } = string.Empty;
         public string Autor { get; set; } = string.Empty;
         public string Genero { get; set; } = string.Empty;
